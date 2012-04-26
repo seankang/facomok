9fdd518e5c4e04f76bfcb8f1150671ae9ad63259
Sean Kang <pwrkr1@gmail.com>
2012-04-26 04:55:31 +0000

    fixed port number

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require("http");
 
-var port = process.env.C9_PORT;
+var port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
