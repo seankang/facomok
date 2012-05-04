cb5e3c36a153a9150da415907428472eeb338c3b
Sean Kang <pwrkr1@gmail.com>
2012-05-04 02:02:26 +0000

    update source

--- app.js
+++ app.js
@@ -3,12 +3,86 @@
  */
 
 var express = require('express')
+    ,io = require("./node_modules/socket.io");
+var app = express.createServer();
+var port = process.env.PORT||80;
+
+// configration
+// can define something, e.g: app.configure('stage', 'prod', function(){ 
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
+    app.register(".html", require("jqtpl").express);    
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
+    app.use(app.router);
+});
+
+// routes
+app.get('/', function(req, res){
+    // set socket io
+    var socket = io.listen(app); 
+    socket.on("connection", function(client) { 
+        client.on("message", function(msg) {
+        	console.info("message : " + msg);
+    	 });  
+    	client.on("disconnect", function() {
+    		consoe.info("disconnected..");
+    	 });  
+    });    
+    
+    // render index page
+    res.render('index', {
+        title: 'Home',
+        port: port,
+        host: 'facomok.seank.c9.io',
+    });
+});
+
+app.listen(port);
+
+/*
+ var http = require("http"), 
+    io = require("./node_modules/socket.io"),       
+    server = http.createServer(function(req, res) {});   
+
+server.listen(8000, null); 
+
+var socket = io.listen(server); 
+socket.on("connection", function(client) { 
+    client.on("message", function(msg) {
+		console.info("message : " + msg);
+	 });  
+	client.on("disconnect", function() {
+		consoe.info("disconnected..");
+	 });  
+});
+
+console.log('Server running at http://127.0.0.1:8000/');
+*/
+
+/*
+app.configure('development', function(){                                            
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    var oneYear = 31557600000;
+    app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+    app.use(express.errorHandler());
+});
+*/
+
+/*
+var express = require('express')
     ,stylus = require('stylus');
 var app = module.exports = express.createServer();
 var port = process.env.PORT||80;
 
 // Configuration
-
 app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
@@ -28,7 +102,6 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', function(req, res){
     res.render('index', {
         title: 'Home'
@@ -48,8 +121,8 @@ app.get('/contact', function(req, res){
 });
 
 // Only listen on $ node app.js
-
 if (!module.parent) {
     app.listen(port);
     console.log("Express server listening on port %d", app.address().port);
 }
+*/
--- public/js/omok.js
+++ public/js/omok.js
@@ -1,6 +1,9 @@
-var MAX_X=16
-    ,MAX_Y=16;
+var MAX_X=16;
+var MAX_Y=16;
 
+var currPlayer=0;
+var gameOver=0;
+var map = new Array();
 
 function createMap()
 {
@@ -13,6 +16,7 @@ function createMap()
         }
     }
 }
+
 function initDisplay() 
 {
     createMap();
@@ -25,8 +29,9 @@ function initDisplay()
     {
         for(j=0; j<map[i].length; j++)
         {
-            outputHtml+="<img src='pan.bmp'  id='id_"+i+"_"+j+"' onMouseDown='fn_onMouseDown("+i+","+j+")' width='20' height='20' style='left:"+moveLeft+"; top:"+moveTop+"; position:absolute;'>";
+            outputHtml+="<img src='/img/pan.bmp'  id='id_"+i+"_"+j+"' onMouseDown='fn_onMouseDown("+i+","+j+")' width='20' height='20' style='left:"+moveLeft+"; top:"+moveTop+"; '>";
             moveLeft+=20;
+            //outputHtml += ' | ' + j;
         }
         
         moveLeft=0;
@@ -34,4 +39,90 @@ function initDisplay()
         outputHtml+="<br>";
     }
     document.write(outputHtml);
+}
+
+function fn_onMouseDown(px,py)
+{
+    if(gameOver==0)
+    {
+        if(map[px][py]!=0)
+        {
+        }
+        else
+        {
+            if(currPlayer==1)
+            {
+                id_1="id_"+px+"_"+py;
+                eval("document.all."+id_1).src="/img/whiteS.bmp";
+                map[px][py]=1;
+                if(check(px,py)==1){ alert('player '+currPlayer+' 승~~'); gameOver=1; }
+                currPlayer=2;
+            }
+            else 
+            {
+                id_1="id_"+px+"_"+py;
+                eval("document.all."+id_1).src="/img/blackS.bmp";
+                map[px][py]=2;
+                 if(check(px,py)==1){ alert('player '+currPlayer+' 승~~'); gameOver=1; }
+                currPlayer=1;
+            }
+            
+        }
+    }
+}
+
+function check(px,py) 
+{ 
+   chk=0;
+   sum = 0; 
+   
+   //가로 체크
+   for(chk=-5; chk<=5; chk++)
+   {
+       if( (py+chk)<0 || (py+chk) >= 16 ){ continue; }
+       if( map[px][py+chk] == currPlayer ){ sum++; if(sum == 5 ){ return 1; } }
+       else{ sum=0; }
+   }
+   
+   sum=0;
+   //세로 체크
+   for(chk=-5; chk<=5; chk++)
+   {
+       if( (px+chk)<0 || (px+chk) >= 16 ){ continue; }
+       if( map[px+chk][py] == currPlayer ){ sum++; if(sum == 5 ){ return 1; } }
+       else{ sum=0; }
+   }
+   
+   //우측 대각선 
+   sum=0;
+   tempX=0;
+   tempY=0;
+   for(chk=-5;chk<=5;chk++){ 
+      if(px-chk < 0 || px-chk >=16){ continue; }
+      if(py+chk < 0 || py+chk >=16){ continue; }
+      
+      if(map[px-chk][py+chk] == currPlayer){ 
+         sum++; 
+         if(sum == 5){ return 1; }
+      } 
+      else{ sum = 0; }   
+      
+   } 
+
+   //좌측 대각선 
+   sum=0;
+   tempX=0;
+   tempY=0;
+   for(chk=-5;chk<=5;chk++){ 
+      if(px+chk < 0 || px+chk >=16){ continue; }
+      if(py+chk < 0 || py+chk >=16){ continue; }
+      
+      if(map[px+chk][py+chk] == currPlayer){ 
+         sum++; 
+         if(sum == 5){ return 1; }
+      } 
+      else{ sum = 0; }       
+   } 
+   
+   return 0; 
 }
